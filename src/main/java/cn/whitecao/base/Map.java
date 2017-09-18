8a0ad4afc67c7a66934c02f5506c0d11f51e2873
腼腆的老黄 <1>
2017-09-18 11:04:55 +0800

    添加hashmap remove方法 偷偷加了部分bug

--- src/main/java/cn/whitecao/base/Map.java
+++ src/main/java/cn/whitecao/base/Map.java
@@ -7,7 +7,7 @@ public interface Map<K,V> {
     //map的基本功能是快速存取
      V put(K k,V v);
      V get(K k);
-
+     V remove(K k);
      public interface Entry<K,V>{
          //map.getKey
          K getKey();
--- src/main/java/cn/whitecao/base/impl/HashMap.java
+++ src/main/java/cn/whitecao/base/impl/HashMap.java
@@ -5,6 +5,7 @@ import cn.whitecao.base.Map;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by 腼腆的老黄 on 2017/9/15.
@@ -20,7 +21,8 @@ public class HashMap<K, V> implements Map<K, V> {
     //定义map
     private Entry<K, V>[] table = null;
     //单独定义一个key=null的entry
-    Entry <K,V> noKeyEntry=new Entry<K, V>(null,null,null);
+    Entry<K, V> noKeyEntry = new Entry<K, V>(null, null, null, 0);
+
     public HashMap() {
         //传说中的门面模式
         this(defaultLength, defaultAddSizeFactory);
@@ -39,55 +41,72 @@ public class HashMap<K, V> implements Map<K, V> {
     }
 
 
-    public V put(K k, V v) {
-        if (k==null){
-            noKeyEntry.v=v;
-            return v;
+    public V put(K key, V value) {
+        if (key == null) {
+            noKeyEntry.v = value;
+            return value;
         }
         if (useSize > defaultLength * defaultAddSizeFactory) {
             up2size();
         }
-        int index = getTableIndex(k, table.length);
+        int hKey = hash(key.hashCode());
+        int index = getTableIndex(hKey, table.length);
         Entry<K, V> entry = table[index];
         if (entry == null) {
             //entry==null说明还没有储存
-            entry = new Entry<K, V>(k, v, null);
+            entry = new Entry<K, V>(key, value, null, hKey);
             table[index] = entry;
             useSize++;
         } else {
             //entry不为空就放到链表头
-            table[index]=new Entry<K, V>(k,v,entry);
-          //  useSize++;
+            table[index] = new Entry<K, V>(key, value, entry, hKey);
+            //  useSize++;
         }
         return table[index].getValue();
     }
 
     public V get(K key) {
-        if (key==null){
-           return noKeyEntry.v;
+        if (key == null) {
+            return noKeyEntry.v;
         }
-        int index = getTableIndex(key, table.length);
-        if (table[index]==null){
+        int hKey = hash(key.hashCode());
+        int index = getTableIndex(hKey, table.length);
+        if (table[index] == null) {
             return null;
         }
         return findValueByEqualKey(key, table[index]);
     }
 
+    public V remove(K key) {
+        if (key == null) {
+            return noKeyEntry.v;
+        }
+        int hKey = hash(key.hashCode());
+        int index = getTableIndex(hKey, table.length);
+        if (table[index] == null) {
+            return null;
+        }
+        removeValueByEqualKey(key, table[index],index);
+        return null;
+    }
     //========================================================================================
+
+
     //通过key和数组长度获取下标长度
-    private int getTableIndex(K k, int length) {
+    private int getTableIndex(int hKey, int length) {
         //length=2^n-1  00001111
         int m = length - 1;
         //index= [0,m);
-        int index = hash(k.hashCode()) & m;
-        return index;
+        // int index = hKey & m;
+        return hKey & m;
     }
 
     /**
      * 扩容
      */
     private void up2size() {
-        Entry[] newTable = new Entry[2 * table.length];
+        defaultLength = defaultLength * 2;
+        Entry[] newTable = new Entry[defaultLength];
         //对扩容前的数组进行散列
         againHash(newTable);
     }
@@ -103,7 +122,6 @@ public class HashMap<K, V> implements Map<K, V> {
         }
         if (entryList.size() > 0) {
             useSize = 0;
-            defaultLength = 2 * defaultLength;
             table = newTable;
             for (Entry<K, V> entry : entryList) {
                 //断开原有的链表
@@ -131,6 +149,36 @@ public class HashMap<K, V> implements Map<K, V> {
     }
 
     /**
+     * 根据key移除entry
+     *
+     * @param key
+     * @param entry
+     */
+    private void removeValueByEqualKey(K key, Entry<K, V> entry,int index) {
+        Entry temp = entry;
+        for (entry=entry;entry!=null;temp = entry,entry=entry.next){
+            if (!key.equals(entry.getKey())) {
+                continue;
+            }
+            //判断是否是头结点
+            if (entry==temp){
+                table[index]=entry.next;
+            }else {
+                //判断是否是尾节点
+                if (null==temp.next.next){
+                    temp.next=null;
+                }else {
+                   temp.next=entry.next;
+                }
+            }
+            break;
+        }
+
+
+
+    }
+
+    /**
      * 递归遍历entry链表,将节点放入entryList中
      *
      * @param entry
@@ -152,34 +200,41 @@ public class HashMap<K, V> implements Map<K, V> {
         return hashCode ^ ((hashCode >>> 20) ^ (hashCode >>> 11));
     }
 
+    static Random random = new Random();
+
     public static void main(String[] args) {
         Map map = new HashMap();
-        for (int i = 0; i < 10000000; i++) {
+        for (int i = 0; i < 1000; i++) {
             map.put("key" + i, i);
+            if (i % 1000000 == 0) {
+                System.out.println(i);
+            }
         }
-        long startTime = System.currentTimeMillis();
-        System.out.println("start:"+startTime);
-
-
-        map.get("key1000000");
-
-
-        long endTime = System.currentTimeMillis();
-        System.out.println("endTime:"+endTime);
-
-        System.out.println(endTime - startTime);
-
+//        map.remove("key684");
+//        map.remove("key563");
+        map.remove("key321");
+        return;
+//        long startTime = System.currentTimeMillis();
+//        System.out.println("start:" + startTime);
+//        for (int i = 0; i < 10000; i++) {
+//            map.get("key1000" + i);
+//        }
+//        long endTime = System.currentTimeMillis();
+//        System.out.println("endTime:" + endTime);
+//        System.out.println(endTime - startTime);
     }
 
     class Entry<K, V> implements Map.Entry<K, V> {
         private K k;
         private V v;
         Entry<K, V> next;
+        int hash;
 
-        public Entry(K k, V v, Entry next) {
+        public Entry(K k, V v, Entry next, int hash) {
             this.k = k;
             this.v = v;
             this.next = next;
+            this.hash = hash;
         }
 
         public K getKey() {
